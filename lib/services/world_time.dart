99f40b402c4dd729e152f18b04c315976fb7e60f
sdMickey <sohamdahiya@gmail.com>
2023-08-25 22:35:13 +0530

    error handling

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -10,27 +10,33 @@ class WorldTime {
   WorldTime({required this.location, required this.flag, required this.url});
 
   Future<void> getTime() async {
-    // make the request
-    Response response =
-        await get(Uri.parse('http://worldtimeapi.org/api/timezone/$url'));
-    Map data = jsonDecode(response.body);
-    // print(data);
+    try {
+      // make the request
+      Response response =
+          await get(Uri.parse('http://worldtimeapi.org/api/timezone/$url'));
+      Map data = jsonDecode(response.body);
+      // print(data);
 
-    // get properties from data
-    String datetime = data['datetime'];
-    // String hour_offset = data['utc_offset'].substring(1, 3);
-    // String minute_offset = data['utc_offset'].substring(4, 6);
-    // print(datetime);
-    // print(hour_offset);
-    // print(minute_offset);
+      // get properties from data
+      String datetime = data['datetime'];
+      // String hour_offset = data['utc_offset'].substring(1, 3);
+      // String minute_offset = data['utc_offset'].substring(4, 6);
+      // print(datetime);
+      // print(hour_offset);
+      // print(minute_offset);
 
-    // create a DateTime object
-    DateTime now = DateTime.parse(datetime.substring(0, 26));
-    // now = now.add(Duration(
-    //     hours: int.parse(hour_offset), minutes: int.parse(minute_offset)));
-    // print(now);
+      // create a DateTime object
+      DateTime now = DateTime.parse(datetime.substring(0, 26));
+      // now = now.add(Duration(
+      //     hours: int.parse(hour_offset), minutes: int.parse(minute_offset)));
+      // print(now);
 
-    // set time property
-    time = now.toString();
+      // set time property
+      time = now.toString();
+    } catch (e) {
+      // ignore: avoid_print
+      print('caught error: $e');
+      time = 'could not get time data';
+    }
   }
 }
