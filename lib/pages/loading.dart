4c8dded09fcdbbea4aa93564a5a1013f2bc431a1
sdMickey <sohamdahiya@gmail.com>
2023-08-25 15:54:20 +0530

    calling worldtimeapi and saving datetime

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -10,18 +10,32 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-  void getData() async {
-    Response response =
-        await get(Uri.parse('https://jsonplaceholder.typicode.com/todos/1'));
+  void getTime() async {
+    // make the request
+    Response response = await get(
+        Uri.parse('http://worldtimeapi.org/api/timezone/Asia/Kolkata'));
     Map data = jsonDecode(response.body);
-    print(data);
-    print(data['title']);
+    // print(data);
+
+    // get properties from data
+    String datetime = data['datetime'];
+    // String hour_offset = data['utc_offset'].substring(1, 3);
+    // String minute_offset = data['utc_offset'].substring(4, 6);
+    // print(datetime);
+    // print(hour_offset);
+    // print(minute_offset);
+
+    // create a DateTime object
+    DateTime now = DateTime.parse(datetime.substring(0, 26));
+    // now = now.add(Duration(
+    //     hours: int.parse(hour_offset), minutes: int.parse(minute_offset)));
+    print(now);
   }
 
   @override
   void initState() {
     super.initState();
-    getData();
+    getTime();
   }
 
   @override
